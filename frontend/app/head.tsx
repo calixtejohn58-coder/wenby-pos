5abd1358c9d887cb0d4f3225f3417e895840f7a8
Wenby MS <calixtejohn58@gmail.com>
2026-05-18 07:33:32 -0700

    add pwa

---
+++ frontend/app/head.tsx
@@ -0,0 +1,33 @@
+export default function Head() {
+
+  return (
+
+    <>
+
+      <title>
+        Wenby POS
+      </title>
+
+      <meta
+        name="theme-color"
+        content="#000000"
+      />
+
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+
+      <link
+        rel="manifest"
+        href="/manifest.json"
+      />
+
+      <link
+        rel="apple-touch-icon"
+        href="/icon-192.png"
+      />
+
+    </>
+  );
+}
